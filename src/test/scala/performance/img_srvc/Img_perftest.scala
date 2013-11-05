2f50b64dd2b3267164bda71ac67fad89fb92ff40
kazimiru <kazimirchyk@gmail.com>
2013-11-05 18:11:35 +0300

    added logging config

--- src/test/scala/performance/img_srvc/Img_perftest.scala
+++ src/test/scala/performance/img_srvc/Img_perftest.scala
@@ -3,28 +3,28 @@ package performance.img_srvc
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
 import scala.concurrent.duration._
-import scala.util.Random
+import scala.concurrent.forkjoin.ThreadLocalRandom
 
 class Img_perftest extends Simulation {
 
   val httpProtocol = http.baseURL("http://nick-intl.mtvnimages-d.mtvi.com")
-  val rand = new Random(System.currentTimeMillis())
+
+  def getRand(i: Int) : String = ThreadLocalRandom.current.nextInt(i).toString
 
   val scn = scenario("Image Server Load test")
-    .feed(csv("LIVE_Load_Testing.csv").random)
+    .feed(csv("LIVE_Load_Testing.csv").circular)
     .exec(http("img_srvc").get("${url}")
-    .queryParam("testParam", rand.nextInt(1000).toString)
+    .queryParam("testParam", _ => getRand(100000))
     .check(status.is(200))
-    //     .check(bodyString.transform(_.map(_.size)).matchWith(io.gatling.core.check.Matchers.greaterThan, 100))
   )
-    .pause(1, 2)
+  .pause(1, 2)
 
   setUp(scn
     .inject(
-    //    rampRate(1 usersPerSec) to (20 usersPerSec) during (30 seconds),
-    constantRate(5 usersPerSec) during (20 seconds)//,
-    //    rampRate(20 usersPerSec) to (1 usersPerSec) during (30 seconds)
-  )
+//        rampRate(1 usersPerSec) to (20 usersPerSec) during (30 seconds),
+        constantRate(1 usersPerSec) during (10 seconds)//,
+//        rampRate(20 usersPerSec) to (1 usersPerSec) during (30 seconds)
+    )
   )
     .protocols(httpProtocol)
 }
