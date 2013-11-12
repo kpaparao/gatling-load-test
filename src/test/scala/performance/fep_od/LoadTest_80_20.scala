ab2efb5d817846f891233b1a258c2abff75c64da
kazimiru <kazimirchyk@gmail.com>
2013-11-12 14:51:42 +0300

    fixed scripts & config

--- src/test/scala/performance/fep_od/LoadTest_80_20.scala
+++ src/test/scala/performance/fep_od/LoadTest_80_20.scala
@@ -3,6 +3,7 @@ package performance.fep_od
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
 import scala.concurrent.duration._
+import bootstrap._
 
 class LoadTest_80_20 extends Simulation {
 
